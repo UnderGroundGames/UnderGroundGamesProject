e93d77a19031f4ecb769277e39aa36fa45978982
TobinTojo <tobintojo@gmail.com>
2021-07-12 17:07:29 -0400

    Implemented Lives System

--- Assets/Scripts/MichaelBall/BallGeneral.cs
+++ Assets/Scripts/MichaelBall/BallGeneral.cs
@@ -5,14 +5,66 @@ using UnityEngine;
 public class BallGeneral : MonoBehaviour
 {
     private Vector3 ballStartPos;
-    
+    private GameObject P1Heart1;
+    private GameObject P1Heart2;
+    private GameObject P1Heart3;
+    private GameObject P2Heart1;
+    private GameObject P2Heart2;
+    private GameObject P2Heart3;
     void Start(){
         ballStartPos = transform.position;
         
         Reset();
         EventScript.current.onResetGame += Reset;
+        P1Heart1 = GameObject.Find("P1 Heart 1");
+        P1Heart2 = GameObject.Find("P1 Heart 2");
+        P1Heart3 = GameObject.Find("P1 Heart 3");
+        P2Heart1 = GameObject.Find("P2 Heart 1");
+        P2Heart2 = GameObject.Find("P2 Heart 2");
+        P2Heart3 = GameObject.Find("P2 Heart 3");
     }
-
+    void Update() {
+	if (HitGround.P1Lives == 3) {
+		P1Heart1.SetActive(true);
+                P1Heart2.SetActive(true);
+                P1Heart3.SetActive(true);
+        }
+        else if (HitGround.P1Lives == 2) {
+		P1Heart1.SetActive(true);
+                P1Heart2.SetActive(true);
+                P1Heart3.SetActive(false);
+        }
+        else if (HitGround.P1Lives == 1) {
+		P1Heart1.SetActive(true);
+                P1Heart2.SetActive(false);
+                P1Heart3.SetActive(false);
+        }
+        else if (HitGround.P1Lives == 0) {
+		P1Heart1.SetActive(false);
+                P1Heart2.SetActive(false);
+                P1Heart3.SetActive(false);
+        }
+        if (HitGround.P2Lives == 3) {
+		P2Heart1.SetActive(true);
+                P2Heart2.SetActive(true);
+                P2Heart3.SetActive(true);
+        }
+        else if (HitGround.P2Lives == 2) {
+		P2Heart1.SetActive(true);
+                P2Heart2.SetActive(true);
+                P2Heart3.SetActive(false);
+        }
+        else if (HitGround.P2Lives == 1) {
+		P2Heart1.SetActive(true);
+                P2Heart2.SetActive(false);
+                P2Heart3.SetActive(false);
+        }
+        else if (HitGround.P2Lives == 0) {
+		P2Heart1.SetActive(false);
+                P2Heart2.SetActive(false);
+                P2Heart3.SetActive(false);
+        }
+    }
     private void Reset(){
         transform.position = ballStartPos;
 	Bounce.bounceSpeed = 8f;
--- Assets/Scripts/ResetBall/HitGround.cs
+++ Assets/Scripts/ResetBall/HitGround.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class HitGround : MonoBehaviour
 {
     public GameObject Ball;
+    public int Player;
+    public static int P1Lives = 3;
+    public static int P2Lives = 3; 
     public enum groundSide{
         Left,
         Right
     };
     public groundSide resetSide;
-
     void OnTriggerEnter2D(Collider2D col){
         if(col.gameObject == Ball){
             EventScript.current.resetGame();
+	    if (Player == 1) {
+                P1Lives -= 1; 
+            }
+            if (Player == 2) {
+                P2Lives -= 1; 
+            }
         }
     }
 }
